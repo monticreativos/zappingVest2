09267b8955fceb4d7ca618abd45b2693166ad921
David Montalba <davidmontalba1985@gmail.com>
2022-06-13 07:32:18 +0200

    up

--- src/app/pages/wather/wather.page.ts
+++ src/app/pages/wather/wather.page.ts
@@ -11,8 +11,45 @@ import { Platform } from '@ionic/angular'
 export class WatherPage implements OnInit {
   title = 'http-get'
   totalAngularPackages: any
-  url: string =
-    'https://api.openweathermap.org/data/2.5/weather?lat=36.7196&lon=-4.42002&appid=388264f755c601e7590e2a681627567e'
+  url = {
+    urlMalaga:
+      'https://api.openweathermap.org/data/2.5/forecast?q=malaga&cnt=7&appid=388264f755c601e7590e2a681627567e&units=metric',
+    urlSabinilla:
+      'https://api.openweathermap.org/data/2.5/weather?lat=36.36728&lon=-5.22785&appid=388264f755c601e7590e2a681627567e',
+    urlManilva:
+      'https://api.openweathermap.org/data/2.5/weather?lat=36.36728&lon=-5.22785&appid=388264f755c601e7590e2a681627567e',
+    urlCasares:
+      'https://api.openweathermap.org/data/2.5/weather?lat=36.36728&lon=-5.22785&appid=388264f755c601e7590e2a681627567e',
+    urlEstepona:
+      'https://api.openweathermap.org/data/2.5/weather?lat=36.36728&lon=-5.22785&appid=388264f755c601e7590e2a681627567e',
+    urlSanPedro:
+      'https://api.openweathermap.org/data/2.5/weather?lat=36.36728&lon=-5.22785&appid=388264f755c601e7590e2a681627567e',
+    urlMarbella:
+      'https://api.openweathermap.org/data/2.5/weather?lat=36.36728&lon=-5.22785&appid=388264f755c601e7590e2a681627567e',
+    urlSotogrande:
+      'https://api.openweathermap.org/data/2.5/weather?lat=36.36728&lon=-5.22785&appid=388264f755c601e7590e2a681627567e',
+    urlAlgecirasna:
+      'https://api.openweathermap.org/data/2.5/weather?lat=36.36728&lon=-5.22785&appid=388264f755c601e7590e2a681627567e',
+    urlCastellar:
+      'https://api.openweathermap.org/data/2.5/weather?lat=36.36728&lon=-5.22785&appid=388264f755c601e7590e2a681627567e',
+    urlTarifa:
+      'https://api.openweathermap.org/data/2.5/weather?lat=36.36728&lon=-5.22785&appid=388264f755c601e7590e2a681627567e',
+    urlVejer:
+      'https://api.openweathermap.org/data/2.5/weather?lat=36.36728&lon=-5.22785&appid=388264f755c601e7590e2a681627567e',
+    urlBarbate:
+      'https://api.openweathermap.org/data/2.5/weather?lat=36.36728&lon=-5.22785&appid=388264f755c601e7590e2a681627567e',
+    urlConil:
+      'https://api.openweathermap.org/data/2.5/weather?lat=36.36728&lon=-5.22785&appid=388264f755c601e7590e2a681627567e',
+    urlChiclana:
+      'https://api.openweathermap.org/data/2.5/weather?lat=36.36728&lon=-5.22785&appid=388264f755c601e7590e2a681627567e',
+    urlCadiz:
+      'https://api.openweathermap.org/data/2.5/weather?lat=36.36728&lon=-5.22785&appid=388264f755c601e7590e2a681627567e',
+    urlJerez:
+      'https://api.openweathermap.org/data/2.5/weather?lat=36.36728&lon=-5.22785&appid=388264f755c601e7590e2a681627567e',
+  }
+
+  peticion: string =
+    'https://api.openweathermap.org/data/2.5/forecast?q=malaga&cnt=7&appid=388264f755c601e7590e2a681627567e&units=metric'
   error: any
 
   ciudad: any
@@ -20,6 +57,8 @@ export class WatherPage implements OnInit {
   temperatura: any
   temperaturaMax: any
   temperaturaMin: any
+  resultados: Array<any> = []
+  resultadosMalaga: any
 
   post: any
   postData: any
@@ -35,23 +74,10 @@ export class WatherPage implements OnInit {
   }
 
   ngOnInit() {
-    this.http.get<any>(this.url).subscribe(
-      (data) => {
-        console.log(data)
-        this.totalAngularPackages = data
-        this.ciudad = data
-        this.icon = data.weather[0]
-        this.temperaturaMax = data.main.temp_max - 273.15
-        this.temperaturaMin = data.main.temp_min - 273.15
-        this.temperatura = data.main.temp - 273.15
-
-        console.log(data.weather[0])
-      },
-      (error) => (this.error = error),
-    )
+    // this.tiempoMalaga()
   }
 
-  onSubmit() {
+  onSubmit(): void {
     // <----
     console.log(
       'onSubmit(): this.totalAngularPackages: ' + this.totalAngularPackages,
@@ -61,5 +87,41 @@ export class WatherPage implements OnInit {
   back() {
     this.post = null
     this.postData.id = null
+  }
+  // tiempoMalaga() {
+  //   this.http.get<any>(this.url.urlMalaga).subscribe(
+  //     (data) => {
+  //       console.log(data)
+  //       this.resultadosMalaga = data
+  //       this.resultados.push(this.resultadosMalaga)
+  //       this.totalAngularPackages = data
+  //       this.ciudad = data
+  //       this.icon = data.weather[0]
+  //       this.temperaturaMax = data.main.temp_max - 273.15
+  //       this.temperaturaMin = data.main.temp_min - 273.15
+  //       this.temperatura = data.main.temp - 273.15
+  //       console.log(this.resultados)
+  //       return data
+  //     },
+  //     (error) => (this.error = error),
+  //   )
+  // }
+
+  searchAction(event) {
+    if (event.detail.value != '') {
+      this.http
+        .get<any>(
+          `https://api.openweathermap.org/data/2.5/forecast?q=${event.detail.value}&cnt=7&appid=388264f755c601e7590e2a681627567e&units=metric`,
+        )
+        .subscribe(
+          (data) => {
+            this.resultados.push(data)
+            console.log(data)
+          },
+          (error) => (this.error = error),
+        )
+    }
+
+    console.log(event)
   }
 }
